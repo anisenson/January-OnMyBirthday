895433ec49bd61961ae75466d255543a75c40366
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-01-25 22:43:08 +0000

    Added Pictures, Finished javascript, Changed html

--- script.js
+++ script.js
@@ -1,3 +1,35 @@
-function Change1883(){
-    
+function Change2007() {
+    document.getElementById('changeImage').src = '#'
+    document.getElementById('Change-Text').innerText = 'This is what happened on 2007'
+    document.getElementById('Change-Date').innerText = 'May 24, 2007'
+}
+
+function Change1883() {
+    document.getElementById('Change-Image').src = 'images/Brooklyn-Bridge.jpg'
+    document.getElementById('Change-Text').innerText = ' The Brooklyn Bridge opened as the longest suspension bridge in the world on May 24, 1883. Spanning across the East River, the Brooklyn Bridge connects the boroughs of Manhattan and Brooklyn in New York.'
+    document.getElementById('Change-Date').innerText = 'May 24, 1883'
+}
+
+function Change1844() {
+    document.getElementById('Change-Image').src = 'images/morse/jpg'
+    document.getElementById('Change-Text').innerText = '1st Telegraph Message 1844 Samuel Morse taps out "What hath God wrought" in the worlds first telegraph message'
+    document.getElementById('Change-Date').innerText = 'May 24th, 1844'
+}
+
+function Change1915() {
+    document.getElementById('Change-Image').src = 'images/Edison.jpg'
+    document.getElementById('Change-Text').innerText = '1915 Thomas Edison invents telescribe to record telephone conversations'
+    document.getElementById('Change-Date').innerText = 'May 24th, 1915'
+}
+
+function Change1930() {
+    document.getElementById('Change-Image').src = 'images/Ruth.jpg'
+    document.getElementById('Change-Text').innerText = '1930 NY Yankees legendary slugger Babe Ruth homers in both games of a doubleheader gives him 9 HRs in one week'
+    document.getElementById('Change-Date').innerText = 'May 24th, 1930'
+}
+
+function Change2014() {
+    document.getElementById('Change-Image').src = 'images/Married.jpg'
+    document.getElementById('Change-Text').innerText = 'Kim Kardashian & Kanye West married. 2014 Rapper and record producer Kanye West (36) weds model Kim Kardashian (33) at Fort di Belvedere in Florence, Italy'
+    document.getElementById('Change-Date').innerText = 'May 24, 2014'
 }
