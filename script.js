ff4488862d57e449dcc8d3ba8f8422e54df9ca33
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-01-31 15:30:44 +0000

    Added Alt text to the pictures

--- script.js
+++ script.js
@@ -2,34 +2,44 @@ function Change2007() {
     document.getElementById('Change-Image').src = 'images/Adam1.jpg' 
     document.getElementById('Change-Text').innerText = 'I was born on May 24th, 2007 at 1:25pm'
     document.getElementById('Change-Date').innerText = 'May 24, 2007'
+    document.getElementById('Change-Image').alt = 'Picture of me when I was younger'
 }
 
 function Change1883() {
     document.getElementById('Change-Image').src = 'images/Brooklyn-Bridge.jpg'
     document.getElementById('Change-Text').innerText = ' The Brooklyn Bridge opened as the longest suspension bridge in the world on May 24, 1883. Spanning across the East River, the Brooklyn Bridge connects the boroughs of Manhattan and Brooklyn in New York.'
     document.getElementById('Change-Date').innerText = 'May 24, 1883'
+    document.getElementById('Change-Image').alt = 'Picture of the Brooklyn Bridge'
 }
 
 function Change1844() {
     document.getElementById('Change-Image').src = 'images/morse.jpg'
     document.getElementById('Change-Text').innerText = '1st Telegraph Message 1844 Samuel Morse taps out "What hath God wrought" in the worlds first telegraph message'
     document.getElementById('Change-Date').innerText = 'May 24, 1844'
+    document.getElementById('Change-Image').alt = 'Picture of Samuel Morse and his first telegraph message'
+
 }
 
 function Change1915() {
     document.getElementById('Change-Image').src = 'images/Edison.jpg'
     document.getElementById('Change-Text').innerText = '1915 Thomas Edison invents telescribe to record telephone conversations'
     document.getElementById('Change-Date').innerText = 'May 24, 1915'
+    document.getElementById('Change-Image').alt = 'Picture of Thomas Edison with his telescribe'
+
 }
 
 function Change1930() {
     document.getElementById('Change-Image').src = 'images/Ruth.jpg'
     document.getElementById('Change-Text').innerText = '1930 NY Yankees legendary slugger Babe Ruth homers in both games of a doubleheader gives him 9 HRs in one week'
     document.getElementById('Change-Date').innerText = 'May 24, 1930'
+    document.getElementById('Change-Image').alt = 'Picture of Babe Ruth swinging his bat'
+
 }
 
 function Change2014() {
     document.getElementById('Change-Image').src = 'images/Married.jpg'
     document.getElementById('Change-Text').innerText = 'Kim Kardashian & Kanye West married. 2014 Rapper and record producer Kanye West (36) weds model Kim Kardashian (33) at Fort di Belvedere in Florence, Italy'
     document.getElementById('Change-Date').innerText = 'May 24, 2014'
+    document.getElementById('Change-Image').alt = 'Picture of Kim Kardashian and Kanye West at their wedding'
+
 }
