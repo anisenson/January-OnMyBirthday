90516ba07f4f161f5a72ae1523da9590cacd1915
anisenson <adamjn524@gmail.com>
2024-01-31 09:29:09 -0500

    Changed Layout of HTML

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function Change2007() {
-    document.getElementById('Change-Image').src = 'images/Ruth.jpg' 
-    document.getElementById('Change-Text').innerText = 'This is what happened on 2007'
+    document.getElementById('Change-Image').src = 'images/Adam1.jpg' 
+    document.getElementById('Change-Text').innerText = 'I was born on May 24th, 2007 at 1:25pm'
     document.getElementById('Change-Date').innerText = 'May 24, 2007'
 }
 
@@ -13,19 +13,19 @@ function Change1883() {
 function Change1844() {
     document.getElementById('Change-Image').src = 'images/morse.jpg'
     document.getElementById('Change-Text').innerText = '1st Telegraph Message 1844 Samuel Morse taps out "What hath God wrought" in the worlds first telegraph message'
-    document.getElementById('Change-Date').innerText = 'May 24th, 1844'
+    document.getElementById('Change-Date').innerText = 'May 24, 1844'
 }
 
 function Change1915() {
     document.getElementById('Change-Image').src = 'images/Edison.jpg'
     document.getElementById('Change-Text').innerText = '1915 Thomas Edison invents telescribe to record telephone conversations'
-    document.getElementById('Change-Date').innerText = 'May 24th, 1915'
+    document.getElementById('Change-Date').innerText = 'May 24, 1915'
 }
 
 function Change1930() {
     document.getElementById('Change-Image').src = 'images/Ruth.jpg'
     document.getElementById('Change-Text').innerText = '1930 NY Yankees legendary slugger Babe Ruth homers in both games of a doubleheader gives him 9 HRs in one week'
-    document.getElementById('Change-Date').innerText = 'May 24th, 1930'
+    document.getElementById('Change-Date').innerText = 'May 24, 1930'
 }
 
 function Change2014() {
