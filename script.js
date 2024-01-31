84822c33fbbd13af48f3596b6770861a6a501fb5
Adam Nisenson <144464774+anisenson@users.noreply.github.com>
2024-01-31 21:34:07 +0000

    Made Mobile Responsiveness, Fixed Grammer

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 function Change2007() {
     document.getElementById('Change-Image').src = 'images/Adam1.jpg' 
-    document.getElementById('Change-Text').innerText = 'I was born on May 24th, 2007 at 1:25pm'
+    document.getElementById('Change-Text').innerText = 'I was born on May 24, 2007 at 1:25pm'
     document.getElementById('Change-Date').innerText = 'May 24, 2007'
     document.getElementById('Change-Image').alt = 'Picture of me when I was younger'
 }
@@ -14,7 +14,7 @@ function Change1883() {
 
 function Change1844() {
     document.getElementById('Change-Image').src = 'images/morse.jpg'
-    document.getElementById('Change-Text').innerText = 'In 1844, Samuel Morse taps out "What hath God wrought" in the worlds first telegraph message'
+    document.getElementById('Change-Text').innerText = 'In 1844, Samuel Morse tapped out "What hath God wrought" in the worlds first telegraph message'
     document.getElementById('Change-Date').innerText = 'May 24, 1844'
     document.getElementById('Change-Image').alt = 'Picture of Samuel Morse and his first telegraph message'
 
@@ -22,7 +22,7 @@ function Change1844() {
 
 function Change1915() {
     document.getElementById('Change-Image').src = 'images/Edison.jpg'
-    document.getElementById('Change-Text').innerText = 'In 1915, Thomas Edison invents the telescribe to record telephone conversations'
+    document.getElementById('Change-Text').innerText = 'In 1915, Thomas Edison invented the telescribe to record telephone conversations'
     document.getElementById('Change-Date').innerText = 'May 24, 1915'
     document.getElementById('Change-Image').alt = 'Picture of Thomas Edison with his telescribe'
 
@@ -30,7 +30,7 @@ function Change1915() {
 
 function Change1930() {
     document.getElementById('Change-Image').src = 'images/Ruth.jpg'
-    document.getElementById('Change-Text').innerText = 'In 1930, NY Yankees legendary slugger Babe Ruth homers in both games of a doubleheader gives him 9 HRs in one week'
+    document.getElementById('Change-Text').innerText = 'In 1930, NY Yankees legendary slugger Babe Ruth homers in both games of a doubleheader giving him 9 HRs in one week'
     document.getElementById('Change-Date').innerText = 'May 24, 1930'
     document.getElementById('Change-Image').alt = 'Picture of Babe Ruth swinging his bat'
 
@@ -38,7 +38,7 @@ function Change1930() {
 
 function Change2014() {
     document.getElementById('Change-Image').src = 'images/Married.jpg'
-    document.getElementById('Change-Text').innerText = 'Kim Kardashian & Kanye West married. 2014 Rapper and record producer Kanye West (36) weds model Kim Kardashian (33) at Fort di Belvedere in Florence, Italy'
+    document.getElementById('Change-Text').innerText = 'Kim Kardashian & Kanye West married in 2014. Rapper and record producer Kanye West (36) weds model Kim Kardashian (33) at Fort di Belvedere in Florence, Italy'
     document.getElementById('Change-Date').innerText = 'May 24, 2014'
     document.getElementById('Change-Image').alt = 'Picture of Kim Kardashian and Kanye West at their wedding'
 
